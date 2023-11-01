5578b81869d8b3ed41bf3375e98d74d9b6876bba
LisethCastillo0325 <lisethcastillo.0325@gmail.com>
2023-11-01 14:16:23 -0500

    set rendering class to JSON

--- crud/settings.py
+++ crud/settings.py
@@ -142,4 +142,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
