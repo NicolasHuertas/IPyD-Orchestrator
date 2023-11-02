ef7bfbf4a8387c2b9327ad6bf7cfe1904a8db8ab
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2023-11-01 22:08:54 -0500

    test push

--- providers/models.py
+++ providers/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=20,decimal_places=2, default=0)
-
+#Provider
 class Provider(models.Model):
     id: models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     emprendedor_id = models.UUIDField(default=uuid.uuid4)
