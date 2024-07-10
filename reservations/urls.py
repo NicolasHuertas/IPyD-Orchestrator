e3049a12d405c064ab9c9fc64d2d06f9d6334baa
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2024-07-10 11:23:45 -0500

    fixed urls

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_reservation, name='create_reservation'),
-    path('cancel/', views.delete_reservation, name='cancel_reservation'),
+    path('cancel/', views.cancel_reservation, name='cancel_reservation'),
 ]
