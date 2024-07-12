9bd6763323e84faa9ddaee2277523e1bdf73bf17
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2024-07-11 21:11:14 -0500

    added view reservation url

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ReservationViewSet.as_view({'get': 'list'}), name='reservations'),
+    path('<int:pk>/', views.ReservationViewSet.as_view({'get': 'retrieve'}), name='reservation'),
     path('create/', views.create_reservation, name='create_reservation'),
     path('cancel/', views.cancel_reservation, name='cancel_reservation'),
 ]
